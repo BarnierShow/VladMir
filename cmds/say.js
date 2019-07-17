092dcb838fccb1def6c224b33a552df78fce9bfa
BarnierShow <52957602+BarnierShow@users.noreply.github.com>
2019-07-17 22:34:54 +0800

    Add files via upload

--- cmds/say.js
+++ cmds/say.js
@@ -1,10 +1,11 @@
 const Discord = module.require("discord.js");
 const fs = require("fs");
 module.exports.run = async (bot,message,args) => {
+    if(!message.member.hasPermission("MENEGE_MESSAGE")) return message.channel.send("```У вас недостаточно прав для исполнения данной комманды```");
     let botmessage = args.join(" ");
     message.delete().catch();
     bot.send(botmessage);
 };
 module.exports.help = {
-    name: "say"
+    name: "say" 
 };
