afda790bcfcbbc2e05a0f72fdecf3b46f34936bb
BarnierShow <52957602+BarnierShow@users.noreply.github.com>
2019-07-31 23:17:24 +0800

    Add files via upload

--- cmds/ping.js
+++ cmds/ping.js
@@ -1,7 +1,7 @@
 const Discord = module.require("discord.js");
 const fs = require("fs");
 module.exports.run = async (bot,message,args) => {
-    message.channel.send('pong!');
+    message.channel.send("Бот запущен, желаю тебе хорошего настроения)");
 };
 module.exports.help = {
     name: "ping"
