4d0ba4fd254e87037f6fae73cea2c8640d51f056
BarnierShow <52957602+BarnierShow@users.noreply.github.com>
2019-08-06 18:02:35 +0800

    Add files via upload

--- cmds/кодер.js
+++ cmds/кодер.js
@@ -4,17 +4,10 @@ module.exports.run = async (bot,message,args) => {
     var embed = new discord.RichEmbed()
     .setColor("#00FCFF")
     .setTitle(`Информация о создатели бота`)
-    .addField(`Имя и Фамилия`, `**Эдуард Харин**`)
-    .addField(`Тег в дискорде`, `**HypeSquad#4704**\n`)
-    .addField(`ВКонтакте`, `**https://vk.com/xdesh**\n`)
-    .addField(`YouTuBe`, `**https://www.youtube.com/channel/UCrH-hu032UV6NvWWsraoTAA?view_as=subscriber**\n`)
-    .addField(`Для заказа бота обрашайтесь сюда:`, `**https://vk.com/xdesh**`)
-    .addField(`Имя и Фамилия`, `**Женя Федоров**`)
-    .addField(`Тег в дискорде`, `**Desert_Gamer#2254**`)
-    .addField(`ВКонтакте`, `**Не известо**`)
-    .addField(`YouTube`, `**Не известно**`)
-    .addField(`Второстепенная информация`, `**Скоро на канале будет видео как он пишет комманду для меня**\n`)
-    .setFooter(`Администрация бота Vlad mir!`, message.guild.iconURL);
+    .addField(`Имя и Фамилия`, `**Неизвестно**`)
+    .addField(`Тег в дискорде`, `**Anonym#4704**\n`)
+    .addField(`ВКонтакте`, `**Неизвестно**\n`)
+    .setFooter(`Администрация Hack.exe!`, message.guild.iconURL);
     message.channel.send(embed);
 };
 module.exports.help = {
